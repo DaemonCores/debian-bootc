196e81338f4b4ee2e372f857877b47ca7d251abf
Pikatsuto <pikatsuto@gmail.com>
2026-07-09 22:05:15 +0200

    refactore sh file

--- workflows/bootc-debs-builder/preinstall/usr/sbin/policy-rc.d
+++ workflows/bootc-debs-builder/preinstall/usr/sbin/policy-rc.d
@@ -1,2 +1,8 @@
 #!/bin/sh
+#
+# policy-rc.d - deny all daemon start/restart actions during the image build.
+# dpkg/apt invoke this hook before (re)starting a service on package install;
+# returning 101 tells the invoke-rc.d helper "forbidden", so no daemon runs
+# inside the build container (there is no init there and services must start
+# only on the real first boot).
 exit 101
